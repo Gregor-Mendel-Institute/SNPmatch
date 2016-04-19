9c75c367d9a89e003fc1a437e03938a222e23479
Pisupati <rahul.pisupati@login1.ice.gmi.oeaw.ac.at>
2016-04-19 09:26:31 +0000

    Setup file similar to imputor

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='SNPmatch',
+    version='1.0',
+    description='A tool to get maximum likely accession in database',
+    long_description=long_description,
+    url='https://github.com/rahbz/snpmatch',
+    author=['Rahul Pisupati'],
+    author_email='rahul.bharadwaj.p@gmail.com',
+    license='GMI',
+    classifiers=[
+        'Development Status :: 1 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='Genotyping Low Coverage sequencing data',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'snpmatch=snpmatch:main'
+        ],
+    },
+)
+
