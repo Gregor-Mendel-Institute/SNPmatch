a6d21aae5ba4bb152e01cc9b81b3c8ff0fec1b7d
rahbz <rahul.bharadwaj.p@gmail.com>
2016-11-16 17:53:44 +0100

    new release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='SNPmatch',
-    version='1.4',
+    version='1.5.0',
     description='A tool to get maximum likely accession in database',
     long_description=long_description,
     url='https://github.com/Gregor-Mendel-Institute/snpmatch',
