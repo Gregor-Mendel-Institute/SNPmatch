720bedb5f33d8d74fbf3779aea8814c73efc361f
rbpisupati <rahul.pisupati@gmi.oeaw.ac.at>
2020-11-10 10:36:29 +0100

    added few functions

--- snpmatch/core/results.py
+++ snpmatch/core/results.py
@@ -37,7 +37,7 @@ class FollowSNPmatch(object):
                 setattr(self, "csmatch_" + req_name + "_fol", os.path.dirname( csv_csmatch[req_name] ) )
         self._instances = pd.Series(self._instances)
     
-    def beauty_print(self, req_name, req_ix = None, req_index = None):
+    def beauty_print(self, req_name, req_ix = None, req_cols = None):
         '''
         Simple function to only print required columns.
         '''
@@ -56,9 +56,9 @@ class FollowSNPmatch(object):
             "RefinedTopHit",
             'RefinedTopHitNumber'
         ])
+        if req_cols:
+            req_beauty_print = pd.Series(req_cols).append(req_beauty_print, ignore_index  = True)
         common_cols = req_beauty_print[req_beauty_print.isin( self.__getattribute__(req_name).columns )]
-        # if req_index:
-        #     return(self.__getattribute__(req_name).loc[ req_ix, common_cols ])
         if req_ix is not None:
             return(self.__getattribute__(req_name).loc[ req_ix, common_cols ])
         return(self.__getattribute__(req_name).loc[:, common_cols ])
@@ -84,6 +84,32 @@ class FollowSNPmatch(object):
                 error_rate = error_rate
             )
 
+    def determine_rank_of_accs(self, req_name, accs_column, snpmatch_fol):
+        """
+        Function to determine choice of accs for a given list of accs in samples
+        input:
+            req_name    : name of the results csv
+            snpmatch_fol: folder where SNPmatch results are there
+            accs_column : name of the column in the dataframe or a pd series
+        output: 
+            added RankofAccs and RankScore to dataframe of req_name
+        """ 
+        if type(accs_column) is str:
+            accs = self.__getattribute__(req_name).loc[:,accs_column]
+        else:
+            # assert accs_column.shape[0] == self.__getattribute__(req_name).shape[0], "provide a pd series with same shape as given dataframe"
+            accs = accs_column
+        files_to_open = accs.shape[0]
+        self.__getattribute__(req_name)['RankofAcc'] = 0
+        self.__getattribute__(req_name)['RankScore'] = np.nan
+        for ef in range(files_to_open):
+            t_file = snpmatch_fol + self.__getattribute__(req_name).loc[accs.index[ef],'FILENAME']
+            t_file_scores = pd.read_csv(t_file + '.scores.txt', header = None, sep = "\t")
+            t_file_scores = t_file_scores.sort_values([5, 3], ascending=[True, False])
+            t_file_scores.iloc[:,0] = t_file_scores.iloc[:,0].astype(str)
+            acc_rank = np.where( t_file_scores.iloc[:,0] == accs[ef] )[0]
+            self.__getattribute__(req_name).loc[accs.index[ef],'RankofAcc'] = acc_rank[0] + 1 
+            self.__getattribute__(req_name).loc[accs.index[ef],'RankScore'] = t_file_scores.iloc[acc_rank[0],3]
 
 
 
