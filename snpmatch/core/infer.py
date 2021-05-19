5998f603960fcd9e17e7a8d4fe607f34f5576a03
rbpisupati <rahul.pisupati@gmi.oeaw.ac.at>
2021-05-19 20:58:14 +0200

    own implementation of viterbi

--- snpmatch/core/infer.py
+++ snpmatch/core/infer.py
@@ -12,6 +12,51 @@ import numpy.ma
 
 log = logging.getLogger(__name__)
 
+# code adapted from Stephen Marsland's, Machine Learning An Algorthmic Perspective, Vol. 2
+# https://github.com/alexsosn/MarslandMLAlgo/blob/master/Ch16/HMM.py
+def viterbi(init_prob, trans_mat, emission_mat, obs):
+    """
+    Parameters:
+        init_prob: initial probabilities
+        trans_mat : transition matrix
+        emission_mat : emission probabilty matrix
+        obs: observations
+    """
+    nStates = np.shape(init_prob)[0]
+    T = np.shape(obs)[0]
+    
+    # init blank path
+    path = np.zeros(T)
+    # delta --> highest probability of any path that reaches state i
+    delta = np.zeros((nStates, T))
+    # phi --> argmax by time step for each state
+    phi = np.zeros((nStates, T))
+    
+    if len(emission_mat.shape) == 2:
+        emission_mat = np.tile( emission_mat.T, (T, 1, 1) ).T
+    
+    # init delta and phi 
+    delta[:, 0] = init_prob * emission_mat[:, obs[0], 0]
+    phi[:, 0] = 0
+
+    # print('\nStart Walk Forward\n')    
+    # the forward algorithm extension
+    for t in range(1, T):
+        for s in range(nStates):
+            delta[s, t] = np.max(delta[:, t-1] * trans_mat[:, s]) * emission_mat[s, obs[t], t] 
+            phi[s, t] = np.argmax(delta[:, t-1] * trans_mat[:, s])
+            # print('s={s} and t={t}: phi[{s}, {t}] = {phi}'.format(s=s, t=t, phi=phi[s, t]))
+    # find optimal path
+    # print('-'*50)
+    # print('Start Backtrace\n')
+    path[T-1] = np.argmax(delta[:, T-1])
+    # p('init path\n    t={} path[{}-1]={}\n'.format(T-1, T, path[T-1]))
+    for t in range(T-2, -1, -1):
+        path[t] = phi[int(path[t+1]), [t+1]]
+        # p(' '*4 + 't={t}, path[{t}+1]={path}, [{t}+1]={i}'.format(t=t, path=path[t+1], i=[t+1]))
+        # print('path[{}] = {}'.format(t, path[t]))
+    return((path, delta, phi))
+
 class IdentifyStrechesofHeterozygosity(object):
 
     def __init__(self,
@@ -40,8 +85,7 @@ class IdentifyStrechesofHeterozygosity(object):
         self.params['recomb_rate'] = recomb_rate
         ## number of iterations
         self.params['n_iter'] = n_iter
-
-        self.model = self._init_hmm_streches_of_het()
+        self._init_hmm_streches_of_het()
 
 
     def _init_hmm_streches_of_het(self):
@@ -56,15 +100,15 @@ class IdentifyStrechesofHeterozygosity(object):
         """
         ### There are two hidden states
         log.info("Initialising HMM")
-        states = ('aa', 'ab')
+        states = ('HOMO', 'HET')
         ## you observe 
-        observations = ('homo', 'het', 'NA')
+        observations = ('00||11', '01', 'NA')
         ## if a site is called Homo -- all the reads mapped to it are the same
         ## het if atleast one read is different (default GATK/bcftools callers does that)
         ##_____
         ## What is the transition probabilities? 
         ## First, I am doing it for F2 cross
-        init_prob = [0.5, 0.5]
+        self.init_prob = [0.5, 0.5]
         ri = (float(self.params['chromosome_size']) / self.params['num_markers']) * self.params['recomb_rate'] / 100
         ## assuming a diploid
         transition_prob = [
@@ -89,12 +133,14 @@ class IdentifyStrechesofHeterozygosity(object):
             [p_homo_given_ZAA,  1 - p_homo_given_ZAA, 0.5],
             [p_homo_given_ZAB, 1 - p_homo_given_ZAB, 0.5]
         ]
-        print("transition probability:\n %s" % pd.DataFrame(transition_prob) )
-        print("emission probability:\n %s" % pd.DataFrame(emission_prob) )
-        model = hmm.MultinomialHMM(n_components=2, n_iter = self.params['n_iter'], algorithm = "viterbi", init_params='st') 
-        model.startprob_ = np.array(init_prob)
-        model.transmat_ = pd.DataFrame(transition_prob)
-        model.emissionprob_ = pd.DataFrame(emission_prob)
+        print("transition probability:\n %s" % pd.DataFrame(transition_prob,index = states, columns = states) )
+        self.transition_prob = np.array(transition_prob)
+        print("emission probability:\n %s" % pd.DataFrame(emission_prob, index = states, columns = observations) )
+        self.emission_prob = np.array(emission_prob)
+        
+    def viterbi(self, obs):
+        log.info("initialising HMM")
+        model = viterbi( self.init_prob, self.transition_prob, self.emission_prob, obs )
         return(model)
 
     @staticmethod
@@ -115,6 +161,8 @@ class IdentifyAncestryF2individual(object):
         recomb_rate = 3.5,  ### in cM/Mb
         error_p1 = 0.00001,
         error_p2 = 0.00001, 
+        hetfrac_p1 = 0.001,
+        hetfrac_p2 = 0.001, 
         base_error = 0.01, 
         avg_depth = 1.5,
         n_iter = 1000
@@ -128,10 +176,12 @@ class IdentifyAncestryF2individual(object):
         self.params['recomb_rate'] = recomb_rate
         self.params['error_p1'] = error_p1
         self.params['error_p2'] = error_p2
+        self.params['hetfrac_p1'] = hetfrac_p1
+        self.params['hetfrac_p2'] = hetfrac_p2
         self.params['base_error'] = base_error
         self.params['avg_depth'] = avg_depth
         self.params['n_iter'] = n_iter
-        self.prob_g_given_z = self._prob_g_given_Z(self.params['error_p1'], self.params['error_p2'] )
+        self.prob_g_given_z = self._prob_g_given_Z(self.params['error_p1'], self.params['error_p2'], self.params['hetfrac_p1'], self.params['hetfrac_p2'] )
         self.prob_x_given_g = self._prob_x_given_G(self.params['base_error'], self.params['avg_depth'] )
         self.init_prob = [0.25, 0.5, 0.25] ### F2 individual with -- Mendelian segregation
         print("init probabilites:\n %s" % pd.Series(self.init_prob, index = self.ancestry) )
@@ -139,16 +189,23 @@ class IdentifyAncestryF2individual(object):
         print("transition probability:\n %s" % self.transition_prob )
         self.emission_prob =  pd.DataFrame(np.dot(self.prob_g_given_z.values, self.prob_x_given_g.values), index = self.ancestry, columns = self.observed_states)
         print("emission probability:\n %s" % self.emission_prob )
-        self.model = self._model( n_iter = self.params['n_iter'] )
 
-    def _prob_g_given_Z(self, error_p1, error_p2):
+    def _prob_g_given_Z(self, error_p1, error_p2, hetfrac_p1, hetfrac_p2):
         ## Adapted from Andolfatto et al.
         conf_p1 = 1 - error_p1
         conf_p2 = 1 - error_p2
+        prob_het_p1 = hetfrac_p1**2
+        prob_het_p2 = hetfrac_p2**2
+        prob_00_given_aa = (conf_p1**2 * (1 - hetfrac_p1)) + (hetfrac_p1 * conf_p1 * error_p1)
+        prob_11_given_aa = (error_p1**2 * (1 - hetfrac_p1)) + (hetfrac_p1 * conf_p1 * error_p1)
+        prob_00_given_bb = (error_p2**2 * (1 - hetfrac_p2)) + (hetfrac_p2 * conf_p2 * error_p2)
+        prob_11_given_bb = (conf_p2**2 * (1 - hetfrac_p2)) + (hetfrac_p2 * conf_p2 * error_p2)
+        prob_00_given_ab = (((1 - hetfrac_p1) * conf_p1) + (hetfrac_p1 * error_p1) ) * (((1 - hetfrac_p2) * error_p2) + (hetfrac_p2 * conf_p2) )
+        prob_11_given_ab = (((1 - hetfrac_p2) * conf_p2) + (hetfrac_p2 * error_p2) ) * (((1 - hetfrac_p1) * error_p1) + (hetfrac_p1 * conf_p1) )
         req_prob = [
-            [conf_p1**2, 2 * conf_p1 * error_p1, error_p1**2],
-            [conf_p1 * error_p2, (conf_p1 * conf_p2)  + (error_p1 * error_p2), conf_p2 * error_p1],
-            [error_p2**2, 2 * conf_p2 * error_p2, conf_p2**2]
+            [prob_00_given_aa, 1 - prob_00_given_aa - prob_11_given_aa, prob_11_given_aa],
+            [prob_00_given_ab, 1 - prob_11_given_ab - prob_00_given_ab, prob_11_given_ab],
+            [prob_00_given_bb, 1 - prob_00_given_bb - prob_11_given_bb, prob_11_given_bb]
         ]
         return( pd.DataFrame( req_prob, index = self.ancestry, columns = self.geno_parents ) )
     
@@ -170,12 +227,9 @@ class IdentifyAncestryF2individual(object):
         ]
         return( pd.DataFrame( transition_prob, index = self.ancestry, columns = self.ancestry ) )
     
-    def _model(self, n_iter = 1000):
+    def viterbi(self, obs, n_iter = 1000):
         log.info("initialising HMM")
-        model = hmm.MultinomialHMM(n_components=3, n_iter = n_iter, algorithm = "viterbi", init_params='ste') 
-        model.startprob_ = self.init_prob
-        model.transmat_ = self.transition_prob
-        model.emissionprob_ = self.emission_prob
+        model = viterbi( self.init_prob, self.transition_prob.values, self.emission_prob.values, obs )
         return(model)
 
     @staticmethod
