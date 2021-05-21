7b8bda23b4dc2dd0efa9687c2e88d107ce769df3
rbpisupati <rahul.pisupati@gmi.oeaw.ac.at>
2021-05-21 16:32:08 +0200

    updated viterbi! few edits on emission mat

--- snpmatch/core/infer.py
+++ snpmatch/core/infer.py
@@ -12,8 +12,8 @@ import numpy.ma
 
 log = logging.getLogger(__name__)
 
-# code adapted from Stephen Marsland's, Machine Learning An Algorthmic Perspective, Vol. 2
-# https://github.com/alexsosn/MarslandMLAlgo/blob/master/Ch16/HMM.py
+# viterbi code adapted from 
+# http://www.adeveloperdiary.com/data-science/machine-learning/implement-viterbi-algorithm-in-hidden-markov-model-using-python-and-r/
 def viterbi(init_prob, trans_mat, emission_mat, obs):
     """
     Parameters:
@@ -22,40 +22,40 @@ def viterbi(init_prob, trans_mat, emission_mat, obs):
         emission_mat : emission probabilty matrix
         obs: observations
     """
-    nStates = np.shape(init_prob)[0]
-    T = np.shape(obs)[0]
-    
-    # init blank path
-    path = np.zeros(T)
-    # delta --> highest probability of any path that reaches state i
-    delta = np.zeros((nStates, T))
-    # phi --> argmax by time step for each state
-    phi = np.zeros((nStates, T))
-    
+    T = obs.shape[0]
+    M = trans_mat.shape[0]
+
     if len(emission_mat.shape) == 2:
         emission_mat = np.tile( emission_mat.T, (T, 1, 1) ).T
-    
-    # init delta and phi 
-    delta[:, 0] = init_prob * emission_mat[:, obs[0], 0]
-    phi[:, 0] = 0
-
-    # print('\nStart Walk Forward\n')    
-    # the forward algorithm extension
+ 
+    omega = np.zeros((T, M))
+    omega[0, :] = np.log(init_prob * emission_mat[:, obs[0],0])
+    prev = np.zeros((T - 1, M))
     for t in range(1, T):
-        for s in range(nStates):
-            delta[s, t] = np.max(delta[:, t-1] * trans_mat[:, s]) * emission_mat[s, obs[t], t] 
-            phi[s, t] = np.argmax(delta[:, t-1] * trans_mat[:, s])
-            # print('s={s} and t={t}: phi[{s}, {t}] = {phi}'.format(s=s, t=t, phi=phi[s, t]))
-    # find optimal path
-    # print('-'*50)
-    # print('Start Backtrace\n')
-    path[T-1] = np.argmax(delta[:, T-1])
-    # p('init path\n    t={} path[{}-1]={}\n'.format(T-1, T, path[T-1]))
-    for t in range(T-2, -1, -1):
-        path[t] = phi[int(path[t+1]), [t+1]]
-        # p(' '*4 + 't={t}, path[{t}+1]={path}, [{t}+1]={i}'.format(t=t, path=path[t+1], i=[t+1]))
-        # print('path[{}] = {}'.format(t, path[t]))
-    return((path, delta, phi))
+        for j in range(M):
+            # Same as Forward Probability
+            probability = omega[t - 1] + np.log(trans_mat[:, j]) + np.log(emission_mat[j, obs[t], t])
+            # This is our most probable state given previous state at time t (1)
+            prev[t - 1, j] = np.argmax(probability)
+            # This is the probability of the most probable state (2)
+            omega[t, j] = np.max(probability)
+ 
+    # Path Array
+    S = np.zeros(T)
+    # Find the most probable last hidden state
+    last_state = np.argmax(omega[T - 1, :])
+ 
+    S[0] = last_state
+ 
+    backtrack_index = 1
+    for i in range(T - 2, -1, -1):
+        S[backtrack_index] = prev[i, int(last_state)]
+        last_state = prev[i, int(last_state)]
+        backtrack_index += 1
+    
+    # Flip the path array since we were backtracking
+    S = np.flip(S, axis=0)
+    return((S, omega))
 
 class IdentifyStrechesofHeterozygosity(object):
 
@@ -66,8 +66,7 @@ class IdentifyStrechesofHeterozygosity(object):
         fraction_homo_parents = 0.99,
         avg_sites_segregating = 0.01, 
         base_error = 0.0001, 
-        recomb_rate = 3.3, 
-        n_iter = 1000
+        recomb_rate = 3.3
     ):
         #  
         self.params = {}
@@ -84,7 +83,6 @@ class IdentifyStrechesofHeterozygosity(object):
         ## recombination rate
         self.params['recomb_rate'] = recomb_rate
         ## number of iterations
-        self.params['n_iter'] = n_iter
         self._init_hmm_streches_of_het()
 
 
@@ -120,26 +118,30 @@ class IdentifyStrechesofHeterozygosity(object):
         # base_error 
         # avg_sites_segregating 
         ## Z -- underlying ancestry -- either AA or AB
-        ## G = genotype at a given locus -- aa or ab -- depends on whether parents are segregating
+        ## G = genotype at a given locus -- 00 or 01 -- depends on whether parents are segregating
         ## X = observed states (00, 01)
         p_homo_given_gaa = ((1 - self.params['base_error']) ** self.params['avg_depth']) + self.params['base_error']**self.params['avg_depth']
         p_homo_given_gab = 2 * (0.5 ** self.params['avg_depth'])
+        prob_x_given_g = np.array([
+            [p_homo_given_gaa, 1 - p_homo_given_gaa, 1],
+            [p_homo_given_gab, 1 - p_homo_given_gab, 1]
+        ])
         ## What fraction of sites are heterozygoues in parental genomes. 
         ## delta_het_parents
-        p_homo_given_ZAA = (self.params['delta_het_parents'] * p_homo_given_gaa ) + ((1 - self.params['delta_het_parents']) * p_homo_given_gab )
-        p_homo_given_ZAB = ((1 - self.params['avg_sites_segregating']) * p_homo_given_gaa ) + ( self.params['avg_sites_segregating'] * p_homo_given_gab )
-
-        emission_prob = [ 
-            [p_homo_given_ZAA,  1 - p_homo_given_ZAA, 0.5],
-            [p_homo_given_ZAB, 1 - p_homo_given_ZAB, 0.5]
-        ]
+        ## Rows are Z -- AA, AB
+        prob_g_given_Z = np.array([
+            [self.params['delta_het_parents'], 1 - self.params['delta_het_parents']],
+            [1 - self.params['avg_sites_segregating'], self.params['avg_sites_segregating']],
+        ])
+        emission_prob = np.dot(prob_g_given_Z, prob_x_given_g)
         print("transition probability:\n %s" % pd.DataFrame(transition_prob,index = states, columns = states) )
         self.transition_prob = np.array(transition_prob)
         print("emission probability:\n %s" % pd.DataFrame(emission_prob, index = states, columns = observations) )
         self.emission_prob = np.array(emission_prob)
         
-    def viterbi(self, obs):
+    def viterbi(self, input_snps):
         log.info("initialising HMM")
+        obs = self.snp_to_observations( input_snps )
         model = viterbi( self.init_prob, self.transition_prob, self.emission_prob, obs )
         return(model)
 
@@ -157,65 +159,102 @@ class IdentifyAncestryF2individual(object):
 
     def __init__(self, 
         chromosome_size,
-        num_markers,
+        snps_p1,
+        snps_p2, 
         recomb_rate = 3.5,  ### in cM/Mb
         error_p1 = 0.00001,
         error_p2 = 0.00001, 
-        hetfrac_p1 = 0.001,
-        hetfrac_p2 = 0.001, 
         base_error = 0.01, 
-        avg_depth = 1.5,
-        n_iter = 1000
+        sample_depth = 1.5
     ):
         self.ancestry = ['AA', 'AB', 'BB']
         self.geno_parents = ['00', '01', '11']
         self.observed_states = ['00', '01', '11', 'NA']
+        print( "observations: %s" % self.observed_states )
         self.params = {}
-        self.params['num_markers'] = num_markers
+        assert snps_p1.shape[0] == snps_p2.shape[0], "both the SNP arrays for two parents should be of same size"
+        self.params['num_markers'] = snps_p1.shape[0]
         self.params['chromosome_size'] = chromosome_size
         self.params['recomb_rate'] = recomb_rate
         self.params['error_p1'] = error_p1
         self.params['error_p2'] = error_p2
-        self.params['hetfrac_p1'] = hetfrac_p1
-        self.params['hetfrac_p2'] = hetfrac_p2
+        self.params['snps_p1'] = snps_p1
+        self.params['snps_p2'] = snps_p2
         self.params['base_error'] = base_error
-        self.params['avg_depth'] = avg_depth
-        self.params['n_iter'] = n_iter
-        self.prob_g_given_z = self._prob_g_given_Z(self.params['error_p1'], self.params['error_p2'], self.params['hetfrac_p1'], self.params['hetfrac_p2'] )
-        self.prob_x_given_g = self._prob_x_given_G(self.params['base_error'], self.params['avg_depth'] )
+        if isinstance(sample_depth, (int, float)):
+            self.params['sample_depth'] = np.repeat(sample_depth, self.params['num_markers'])
+        else:
+            self.params['sample_depth'] = sample_depth
         self.init_prob = [0.25, 0.5, 0.25] ### F2 individual with -- Mendelian segregation
         print("init probabilites:\n %s" % pd.Series(self.init_prob, index = self.ancestry) )
         self.transition_prob = self._transition_prob(self.params['chromosome_size'], self.params['num_markers'], self.params['recomb_rate'])
         print("transition probability:\n %s" % self.transition_prob )
-        self.emission_prob =  pd.DataFrame(np.dot(self.prob_g_given_z.values, self.prob_x_given_g.values), index = self.ancestry, columns = self.observed_states)
-        print("emission probability:\n %s" % self.emission_prob )
+        print("calculating emissions")
+        self.emission_prob = self._get_emissions(self.params['error_p1'], self.params['error_p2'], self.params['snps_p1'], self.params['snps_p2'], self.params['base_error'], self.params['sample_depth'])
 
-    def _prob_g_given_Z(self, error_p1, error_p2, hetfrac_p1, hetfrac_p2):
+    def _get_emissions(self, error_p1, error_p2, snps_p1, snps_p2, base_error, sample_depth):
+        unique_p1 = np.unique(snps_p1)
+        unique_p2 = np.unique(snps_p2)
+        emission_prob = np.zeros( (len(self.ancestry), len(self.observed_states), len(snps_p1)) )
+        iter_parameter = pd.DataFrame( { "snps_p1": snps_p1, "snps_p2": snps_p2, "dp": sample_depth } ).drop_duplicates()
+        for ef_param in iter_parameter.iterrows(): 
+            ef_emission = self._calc_emission_given_af( 
+                error_p1, 
+                error_p2, 
+                get_af(ef_param[1]['snps_p1']), 
+                get_af(ef_param[1]['snps_p2']), 
+                base_error, 
+                ef_param[1]['dp'] 
+            )
+            req_snp_ix = np.where((snps_p1 == ef_param[1]['snps_p1']) & ( snps_p2 == ef_param[1]['snps_p2'] ) & (sample_depth == ef_param[1]['dp']) )[0]
+            # for ef_ix in req_snp_ix:
+                # emission_prob[:,:,ef_ix] = ef_emission.values
+            emission_prob[:,:,req_snp_ix] = np.tile( ef_emission.values.T, (len(req_snp_ix), 1, 1) ).T
+        return( emission_prob )
+    
+    def _calc_emission_given_af(self, error_p1, error_p2, af_p1, af_p2, base_error, avg_depth):
+        """
+        Calculate emission probability at a given marker with
+        error_p* : estimate for sequencing error for parent *
+        af_p* : allele frequncy for two alleles (say reference and alternative) for the parent
+                for example: 0 for homo (00), 0.5 for het (01) and 1 for homo (11)
+        base_error : base error for the sample
+        avg_depth: depth for the sample at a given position
+        """
         ## Adapted from Andolfatto et al.
         conf_p1 = 1 - error_p1
         conf_p2 = 1 - error_p2
-        prob_het_p1 = hetfrac_p1**2
-        prob_het_p2 = hetfrac_p2**2
-        prob_00_given_aa = (conf_p1**2 * (1 - hetfrac_p1)) + (hetfrac_p1 * conf_p1 * error_p1)
-        prob_11_given_aa = (error_p1**2 * (1 - hetfrac_p1)) + (hetfrac_p1 * conf_p1 * error_p1)
-        prob_00_given_bb = (error_p2**2 * (1 - hetfrac_p2)) + (hetfrac_p2 * conf_p2 * error_p2)
-        prob_11_given_bb = (conf_p2**2 * (1 - hetfrac_p2)) + (hetfrac_p2 * conf_p2 * error_p2)
-        prob_00_given_ab = (((1 - hetfrac_p1) * conf_p1) + (hetfrac_p1 * error_p1) ) * (((1 - hetfrac_p2) * error_p2) + (hetfrac_p2 * conf_p2) )
-        prob_11_given_ab = (((1 - hetfrac_p2) * conf_p2) + (hetfrac_p2 * error_p2) ) * (((1 - hetfrac_p1) * error_p1) + (hetfrac_p1 * conf_p1) )
-        req_prob = [
+        prob_00_given_aa = (conf_p1**2 * (1 - af_p1)) + (error_p1**2 * af_p1)
+        prob_11_given_aa = (conf_p1**2 * af_p1) + (error_p1**2 * (1 - af_p1))
+
+        prob_00_given_bb = (conf_p2**2 * (1 - af_p2)) + (error_p2**2 * af_p2)
+        prob_11_given_bb = (conf_p2**2 * af_p2) + (error_p2**2 * (1 - af_p2))
+
+        prob_00_given_ab = (((1-af_p1) * conf_p1) + (af_p1 * error_p1) ) * (((1 - af_p2) * conf_p2) + (af_p2 * error_p2) )
+        prob_11_given_ab = ((af_p1 * conf_p1) + ((1-af_p1) * error_p1) ) * ((af_p2 * conf_p2) + ((1 - af_p2) * error_p2))
+
+        prob_g_given_Z = [
             [prob_00_given_aa, 1 - prob_00_given_aa - prob_11_given_aa, prob_11_given_aa],
             [prob_00_given_ab, 1 - prob_11_given_ab - prob_00_given_ab, prob_11_given_ab],
             [prob_00_given_bb, 1 - prob_00_given_bb - prob_11_given_bb, prob_11_given_bb]
         ]
-        return( pd.DataFrame( req_prob, index = self.ancestry, columns = self.geno_parents ) )
-    
-    def _prob_x_given_G(self, base_error, avg_depth):
-        req_prob = [
-            [(1 - base_error)**avg_depth, 1 - base_error**avg_depth - (1 - base_error)**avg_depth, base_error**avg_depth, 0.5],
-            [(0.5 * (1 - base_error))**avg_depth, 1 - 2 * ((0.5 * (1 - base_error))**avg_depth),(0.5 * (1 - base_error))**avg_depth,0.5],
-            [base_error**avg_depth, 1 - base_error**avg_depth - (1 - base_error)**avg_depth, (1-base_error)**avg_depth, 0.5]
+        ## if depth is 2 -- p(00|00) = 0.99**2 , P(11|00) = 0.01**2 
+        p_00_given_g00 = (1 - base_error)**avg_depth ## if depth is 10 -- 0.99 ** 10
+        p_11_given_g00 = base_error ** avg_depth
+        p_01_given_g00 = 1 - p_00_given_g00 - p_11_given_g00
+        # qbe = 0.5*base_error
+        # qb = 0.5*(1-base_error)
+        # p_00_given_g01 = (0.5)**avg_depth 
+        #  (avg_depth,0) qb**avg_depth + (avg_depth,1) (qb**(avg_depth-1) * qbe) + avg_depth C2 (qb**(avg_depth-2) * qbe**2)
+        p_01_given_g01 = 1 - 2 * (0.5**avg_depth)
+        p_00_given_g01 = (1 - p_01_given_g01)/2
+        p_00_given_g01 = (1 - p_01_given_g01)/2
+        prob_x_given_g = [
+            [p_00_given_g00, p_01_given_g00, p_11_given_g00, 1],
+            [p_00_given_g01, p_01_given_g01, p_00_given_g01, 1],
+            [p_11_given_g00, p_01_given_g00, p_00_given_g00, 1]
         ]
-        return( pd.DataFrame( req_prob, index = self.geno_parents, columns = self.observed_states )  )
+        return( pd.DataFrame(np.dot(np.array(prob_g_given_Z), np.array(prob_x_given_g)), index = self.ancestry, columns = self.observed_states) ) 
 
     def _transition_prob(self, chromosome_size, num_markers, recomb_rate):
         ri = (float(chromosome_size) / num_markers) * recomb_rate / 100
@@ -227,33 +266,55 @@ class IdentifyAncestryF2individual(object):
         ]
         return( pd.DataFrame( transition_prob, index = self.ancestry, columns = self.ancestry ) )
     
-    def viterbi(self, obs, n_iter = 1000):
+    def viterbi(self, input_snps):
         log.info("initialising HMM")
-        model = viterbi( self.init_prob, self.transition_prob.values, self.emission_prob.values, obs )
+        obs = self.snp_to_observations( input_snps )
+        model = viterbi( self.init_prob, self.transition_prob.values, self.emission_prob, obs )
         return(model)
 
     @staticmethod
-    def snp_to_observations(input_snps, snpsP1_gt, snpsP2_gt, polarize = None):
+    def snp_to_observations(input_snps):
         # input_snps = parsers.parseGT(input_gt)
         num_snps = len(input_snps)
         ### Here I have 4 observed states
         ## ('00', '01', '11',  'NA') ### removed '0', '1',
         ##    0,    1,    2,   3  ###  4,    5
-        ebPolarised = np.repeat(3, num_snps)
-        input_snps_mask = numpy.ma.masked_less(input_snps, 0)
-        snpsP1_gt_mask = numpy.ma.masked_less(numpy.ma.masked_greater(snpsP1_gt, 1), 0)
-        snpsP2_gt_mask = numpy.ma.masked_less(numpy.ma.masked_greater(snpsP2_gt, 1), 0)
-        if polarize == "p1":
-            ebPolarised[np.where((np.equal( input_snps_mask, snpsP1_gt_mask )) )[0] ] = 0  ## 00
-            ebPolarised[np.where((~np.equal( input_snps_mask, snpsP1_gt_mask )) & (input_snps_mask < 2) )[0] ] = 2  ## 11
-        elif polarize == "p2":
-            ebPolarised[np.where((np.equal( input_snps_mask, snpsP2_gt_mask )) )[0] ] = 2  ## 00
-            ebPolarised[np.where((~np.equal( input_snps_mask, snpsP2_gt_mask )) & ( input_snps_mask < 2 ) )[0] ] = 0  ## 11
-        else:
-            ebPolarised[np.where((np.equal( input_snps_mask, snpsP1_gt_mask )) )[0] ] = 0  ## 00
-            ebPolarised[np.where((np.equal( input_snps_mask, snpsP2_gt_mask )) )[0] ] = 2  ## 11
-        ebPolarised[np.where( np.equal( input_snps_mask, np.repeat(2, num_snps)) & (snpsP1_gt_mask != snpsP2_gt_mask ) )[0] ] = 1
-        return(ebPolarised)
+        t_sample_snps = np.copy(input_snps)
+        t_sample_snps[t_sample_snps == -1] = 3
+        t_sample_snps[t_sample_snps == 2] = 5
+        t_sample_snps[t_sample_snps == 1] = 2
+        t_sample_snps[t_sample_snps == 5] = 1
+        return(t_sample_snps)
+
+def get_af(snps):
+    t_af = np.copy(snps)
+    t_af[t_af == 1] = 4
+    t_af[t_af == 2] = 1
+    t_af[t_af == 4] = 2
+    t_af = t_af / 2
+    return(t_af)
+
+def polarize_snps(input_snps, snps_p1, snps_p2, polarize_to = None):
+    # input_snps = parsers.parseGT(input_gt)
+    num_snps = len(input_snps)
+    ### Here I have 4 observed states
+    ## ('00', '01', '11',  'NA') ### removed '0', '1',
+    ##    0,    1,    2,   3  ###  4,    5
+    ebPolarised = np.repeat(3, num_snps)
+    input_snps_mask = numpy.ma.masked_less(input_snps, 0)
+    snpsP1_gt_mask = numpy.ma.masked_less(numpy.ma.masked_greater(snps_p1, 1), 0)
+    snpsP2_gt_mask = numpy.ma.masked_less(numpy.ma.masked_greater(snps_p2, 1), 0)
+    if polarize_to == "p1":
+        ebPolarised[np.where((np.equal( input_snps_mask, snpsP1_gt_mask )) )[0] ] = 0  ## 00
+        ebPolarised[np.where((~np.equal( input_snps_mask, snpsP1_gt_mask )) & (input_snps_mask < 2) )[0] ] = 2  ## 11
+    elif polarize_to == "p2":
+        ebPolarised[np.where((np.equal( input_snps_mask, snpsP2_gt_mask )) )[0] ] = 2  ## 00
+        ebPolarised[np.where((~np.equal( input_snps_mask, snpsP2_gt_mask )) & ( input_snps_mask < 2 ) )[0] ] = 0  ## 11
+    else:
+        ebPolarised[np.where((np.equal( input_snps_mask, snpsP1_gt_mask )) )[0] ] = 0  ## 00
+        ebPolarised[np.where((np.equal( input_snps_mask, snpsP2_gt_mask )) )[0] ] = 2  ## 11
+    ebPolarised[np.where( np.equal( input_snps_mask, np.repeat(2, num_snps)) & (snpsP1_gt_mask != snpsP2_gt_mask ) )[0] ] = 1
+    return(ebPolarised)
 
 
 def uniq_neighbor(a):
